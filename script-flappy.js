62ab1db2f829ee3574c6d84819b3dccbec1ce194
Yaiza Rubio <yaizarubio@Yaizas-MacBook-Pro.local>
2021-12-31 17:54:03 +0100

    add calico square

--- script-flappy.js
+++ script-flappy.js
@@ -1,6 +1,6 @@
 import { getRandomNumber } from '/utils/utils.js'
 
-let world, block, hole, calico, score
+let world, block, hole, calico, star, score
 let velocidad = 50;
 let mLeft = 0;
 let mTop = 0;
@@ -11,6 +11,7 @@ function getElements() {
     hole = document.querySelector( '.hole' )
     calico = document.querySelector( '.calico-bird' )
     score = document.querySelector( '.score' )
+    star = document.querySelector('.star')
 }
 document.addEventListener("keydown", function(e){
     if (e.keyCode == "39"){
@@ -27,21 +28,32 @@ document.addEventListener("keydown", function(e){
     }
 })
 
-
 function moverDerecha(){
     mLeft += velocidad;
+    if (mLeft > 400){
+        mLeft = 400
+    }
     calico.style.marginLeft = mLeft + "px"
 }
 function moverIzquierda(){
     mLeft -= velocidad;
+    if (mLeft < -350){
+        mLeft = -350
+    }
     calico.style.marginLeft = mLeft + "px"
 }
 function moverArriba(){
     mTop -= velocidad;
+    if (mTop < -450){
+        mTop = -450
+    }
     calico.style.top = mTop + "px"
 }
 function moverAbajo(){
     mTop += velocidad;
+    if (mTop > 0){
+        mTop = 0
+    }
     calico.style.top = mTop + "px"
 }
 function initRandomHoles() {
